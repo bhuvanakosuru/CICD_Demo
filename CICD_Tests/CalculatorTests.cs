60320f5231515f57d05cefb5fa303d660e9c980d
Victor Guo <guo37@purdue.edu>
2025-02-10 05:48:29 +0000

    restored to initial state

--- CICD_Tests/CalculatorTests.cs
+++ CICD_Tests/CalculatorTests.cs
@@ -1,19 +1,21 @@
+using CICD_Core;
+
 namespace CICD_Tests
 {
     public class CalculatorTests
     {
         [Fact]
-        public void Add_TwoNumbers_ReturnsCorrectSum()
+        public void Divide_TwoNumbers_ReturnsCorrectQuotient()
         {
-            int result = Calculator.Add(2, 3);
+            double result = Calculator.Divide(10, 2);
             Assert.Equal(5, result);
         }
 
         [Fact]
-        public void Add_NegativeNumbers_ReturnsIncorrectSum() // Issue: This test is wrong intentionally
+        public void Divide_NegativeNumbers_ReturnsCorrectQuotient()
         {
-            int result = Calculator.Add(-2, -3);
-            Assert.Equal(-6, result); // Issue: Should be -5
+            double result = Calculator.Divide(-10, -2);
+            Assert.Equal(5, result);
         }
     }
 }
