99ad967f8b2c778804c960c3acdc4ef6113c6be4
Victor Guo <guosite@gmail.com>
2025-02-10 20:14:23 +0000

    fixed issues

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,8 +1,35 @@
-﻿using CICD_Core;
+﻿using System.ComponentModel;
+using CICD_Core;
 
 Console.WriteLine("Enter the first number:");
-int a = int.Parse(Console.ReadLine());
+int a, b;
+while (true)
+{
+    string? s = Console.ReadLine();
+    
+    if (!int.TryParse(s, out a))
+    {
+        Console.WriteLine("Error: please enter an integer");
+    }
+    else
+    {
+        break;
+    }
+}
+
 Console.WriteLine("Enter the second number:");
-int b = int.Parse(Console.ReadLine());
+while (true)
+{
+    string? s = Console.ReadLine();
+    if (!int.TryParse(s, out b))
+    {
+        Console.WriteLine("Error: please enter an integer");
+    }
+    else
+    {
+        break;
+    }
+}
+
 double res = Calculator.Divide(a, b);
 Console.WriteLine($"{a} / {b} = {res}");
