99aa6b99207bf4e8d557f70a1e10c0ba4934a4ea
Victor Guo <guo37@purdue.edu>
2025-02-10 19:23:28 +0000

    divide completed

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CICD_Core;
+
+Console.WriteLine("Enter the first number:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int b = int.Parse(Console.ReadLine());
+double res = Calculator.Divide(a, b);
+Console.WriteLine($"{a} / {b} = {res}");
---
+++ CICD_Core/Calculator.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CICD_Core
+{
+    public class Calculator
+    {
+        public static double Divide(int v1, int v2)
+        {
+            int a = 3;
+            if (v2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)v1/v2;
+        }
+    }
+}
